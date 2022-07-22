33859f4d291151ebd93340357f214ce0b5504e4e
Khushboo Chaudhary <khushboo.chaudhary@jellyfishtechnologies.com>
2022-07-22 17:55:49 +0530

    final changes done

--- two.js
+++ two.js
@@ -1,36 +1,37 @@
 function selectStation() {
-    let a = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28'];
+    let a = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'M', 'O',
+        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     for (let i = 0; i <= a.length - 1; i++) {
-        //console.log(a[i])
-        document.getElementById("Source").innerHTML() += `<option>${a[i]}</option>`;
-        document.getElementById('Destination').innerHTML() += `<option>${a[i]}</option>`;
+        document.getElementById("Destination").innerHTML += `<option value=${i + 1}>${a[i]}</option>`;
+        document.getElementById("xyz").innerHTML += `<option value=${i + 1}>${a[i]}</option>`;
     }
-
 }
 selectStation();
 
 
+function calculateFair() {
+    let sourValue = document.getElementById('xyz').value;
+    let destValue = document.getElementById('Destination').value;
+    let noofstat = Math.abs(sourValue - destValue);
 
-// let a = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28'];
-// for (let i = 0; i <= a.length - 1; i++) {
-//     document.getElementById("Source").innerHTML = `<option>${a[i]}</option>`;
-//     document.getElementById('Destination').innerHTML = `<option>${a[i]}</option>`;
-// }
-// var option = document.createElement("option");
-// option.text = "Text";
-// option.value = "myvalue";
-// var select = document.getElementById("id-to-my-select-box");
-// select.appendChild(option);
+    let fair = 0;
+    if (noofstat % 4 == 0) {
+        fair = (Math.floor(noofstat / 4)) * 10;
+    }
+    else {
+        fair = (Math.floor(noofstat / 4) + 1) * 10;
+    }
+    if (noofstat > 24) {
+        fare = 70;
+    }
+    console.log(fair);
 
-//let a = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28'];
-// const source = document.querySelector("#Source");
-// const destination = document.getElementById('#Destination');
+    if (document.getElementById("Boxes").value == "Card" && noofstat >= 5) {
+        fair = fair - (10 / 100 * fair);
+    }
+    if (noofstat < 1) {
+        fair = 10;
+    }
 
-// for (let i = 1; i <= 28; i++) {
-//     let option = document.createElement("option");
-//     option.text = i;
-//     option.value = i;
-//     source.appendChild(option);
-//     destination.appendChild(option);
-
-// }
+    document.getElementById('fairBox').value = fair;
+}
